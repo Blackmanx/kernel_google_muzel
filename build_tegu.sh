62a149234b532252724a92e057c79c040036652a
Blackmanx <blackman@sajl.cc>
2025-12-27 02:46:46 +0100

    ZP11.251031.009: Update build_tegu.sh

--- build_tegu.sh
+++ build_tegu.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-# SPDX-License-Identifier: GPL-2.0
-
-exec tools/bazel run \
-    --config=stamp \
-    --config=tegu \
-    //private/devices/google/tegu:zumapro_tegu_dist "$@"
+private/devices/google/tegu/build_tegu.sh
