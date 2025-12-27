b1be2a680ba02ca438083cc908c0a1c7515f9c64
Blackmanx <blackman@sajl.cc>
2025-12-27 02:43:44 +0100

    ZP11.251031.009: Update build_pantah.sh

--- build_pantah.sh
+++ build_pantah.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-# SPDX-License-Identifier: GPL-2.0
-
-exec tools/bazel run \
-    --config=stamp \
-    --config=pantah \
-    //private/devices/google/pantah:gs201_pantah_dist "$@"
+private/devices/google/pantah/build_pantah.sh
