c0c8f1319aaa57ad71c7d2282b43c550915ffd2a
Blackmanx <blackman@sajl.cc>
2025-12-27 02:43:33 +0100

    ZP11.251031.009: Update build_caimito.sh

--- build_caimito.sh
+++ build_caimito.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-# SPDX-License-Identifier: GPL-2.0
-
-exec tools/bazel run \
-    --config=stamp \
-    --config=caimito \
-    //private/devices/google/caimito:zumapro_caimito_dist "$@"
+private/devices/google/caimito/build_caimito.sh
