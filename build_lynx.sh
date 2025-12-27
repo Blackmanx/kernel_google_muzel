09fd6fd1bc05367c77397b4118c3fd02fe4b7309
Blackmanx <blackman@sajl.cc>
2025-12-27 02:43:41 +0100

    ZP11.251031.009: Update build_lynx.sh

--- build_lynx.sh
+++ build_lynx.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-# SPDX-License-Identifier: GPL-2.0
-
-exec tools/bazel run \
-    --config=stamp \
-    --config=lynx \
-    //private/devices/google/lynx:gs201_lynx_dist "$@"
+private/devices/google/lynx/build_lynx.sh
