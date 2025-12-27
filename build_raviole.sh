1ce5bb21cfc5920674423b161c78c33b175a5e78
Blackmanx <blackman@sajl.cc>
2025-12-27 02:46:46 +0100

    ZP11.251031.009: Update build_raviole.sh

--- build_raviole.sh
+++ build_raviole.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-# SPDX-License-Identifier: GPL-2.0
-
-exec tools/bazel run \
-    --config=stamp \
-    --config=raviole \
-    //private/devices/google/raviole:gs101_raviole_dist "$@"
+private/devices/google/raviole/build_raviole.sh
