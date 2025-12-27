e56489bb4232087a3fa74762e0afbd89a80ca22c
Blackmanx <blackman@sajl.cc>
2025-12-27 02:46:46 +0100

    ZP11.251031.009: Update build_zumapro.sh

--- build_zumapro.sh
+++ build_zumapro.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-# SPDX-License-Identifier: GPL-2.0-only
-
-exec tools/bazel run \
-    --config=stamp \
-    --config=zumapro \
-    //private/devices/google/zumapro:dist "$@"
+private/devices/google/zumapro/build_zumapro.sh
