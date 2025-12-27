0635eaa6e46fcbded80726993b693f5d421dd112
Blackmanx <blackman@sajl.cc>
2025-12-27 02:43:38 +0100

    ZP11.251031.009: Update build_gs101.sh

--- build_gs101.sh
+++ build_gs101.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-# SPDX-License-Identifier: GPL-2.0-only
-
-exec tools/bazel run \
-    --config=stamp \
-    --config=gs101 \
-    //private/devices/google/gs101:dist "$@"
+private/devices/google/gs101/build_gs101.sh
