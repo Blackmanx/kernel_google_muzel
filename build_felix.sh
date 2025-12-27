562459aff9b08eb0068f07f410c34cce40bbed6d
Blackmanx <blackman@sajl.cc>
2025-12-27 02:43:37 +0100

    ZP11.251031.009: Update build_felix.sh

--- build_felix.sh
+++ build_felix.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-# SPDX-License-Identifier: GPL-2.0
-
-exec tools/bazel run \
-    --config=stamp \
-    --config=felix \
-    //private/devices/google/felix:gs201_felix_dist "$@"
+private/devices/google/felix/build_felix.sh
