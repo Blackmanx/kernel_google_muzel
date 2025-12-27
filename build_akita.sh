7eb39f6d8bb8e002d17d35888f78fc360589c51a
Blackmanx <blackman@sajl.cc>
2025-12-27 02:43:31 +0100

    ZP11.251031.009: Update build_akita.sh

--- build_akita.sh
+++ build_akita.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-# SPDX-License-Identifier: GPL-2.0
-
-exec tools/bazel run \
-    --config=stamp \
-    --config=akita \
-    //private/devices/google/akita:zuma_akita_dist "$@"
+private/devices/google/akita/build_akita.sh
