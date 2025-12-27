2a9f8558fe57e7cd44f659e698a3df59c195671b
Blackmanx <blackman@sajl.cc>
2025-12-27 02:43:32 +0100

    ZP11.251031.009: Update build_bluejay.sh

--- build_bluejay.sh
+++ build_bluejay.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-# SPDX-License-Identifier: GPL-2.0
-
-exec tools/bazel run \
-    --config=stamp \
-    --config=bluejay \
-    //private/devices/google/bluejay:gs101_bluejay_dist "$@"
+private/devices/google/bluejay/build_bluejay.sh
